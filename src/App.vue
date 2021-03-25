520662be5b390901f946472fc10771ff3dec6cd3
ios_app <a88@163.com>
2021-03-25 17:13:24 +0800

    first and vue

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Button v-bind:list="list" v-on:setValue="setValue" />
+    <button-a ></button-a>
+    <button-c></button-c>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button'
+import ButtonA from './components/ButtonA'
+import ButtonC from './components/ButtonC'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    Button,
+    ButtonA,
+    ButtonC
+  },
+  data(){
+    return{
+      list: [{name: 'java',id: 1001},{name: 'python',id: 1002},{name: 'javascript',id: 1003}]
+    }
+  },
+  methods: {
+    setValue(e){
+      console.log('子像氟元素传递数据',e)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+button-a{
+  margin-bottom: 10px;
+  margin-top: 10px;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <a-button type="primary" @click="changePerson">BUTTONb</a-button>
+        <!-- <span>{{person.a.b.c.d.f.e.g.h}}</span> -->
+        <!-- <p>{{person.b || '---'}}</p> -->
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        list: {
+            type: Array
+        }
+    },
+    beforeCreate(){
+        console.log('beforeCreate');
+    },
+    created(){
+        console.log('created!!!')
+        console.log('list parent',this.list);
+    },
+    data(){
+        return {
+            person: {
+                a:{
+                    b: {
+                        c:{
+                            d: {
+                                f:{e:{
+                                    g: {
+                                        h:123
+                                    }
+                                }}
+                            }
+                        }
+                    }
+                }
+            },
+            buonname:'我是b'
+
+        }
+    },
+    methods: {
+        changePerson: function(){
+            console.log(this.person);
+            this.person.a.b.c.d.f.e.g.h = 456;
+            this.person.b = 'new 123';
+            this.$emit('setValue',{name: 'zhangsan',num: 20})
+            this.$EventBus.$emit('btn-B',this.buonname);        }
+    }
+    
+}
+</script>
+<style>
+
+</style>
